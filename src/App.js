8ecd2d0e2d0a11f81ef2877454d5ec9aacf37af7
aliiev <aliiev.akhtem@gmail.com>
2022-06-26 02:33:13 +0300

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Navbar from './components/Navbar'
+
 const App = () => (
-  <div className="container mx-auto mt-12">
-    <h2 className="text-6xl font-bold">Hello world</h2>
-    <p className="text-base font-serif">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum, natus. Dolorem aliquam laborum, tempore quia aperiam maxime, cumque ipsam reprehenderit blanditiis voluptas aut nihil nesciunt possimus, cupiditate aliquid sint doloribus!</p>
+  <div className="container mx-auto mt-8 px-6 md:px-0">
+    <Navbar />
   </div>
 )
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import Logo from '../resources/Logo.svg'
+
+const Navbar = () => (
+  <nav className="flex justify-between items-center w-full">
+    <a className="rounded-full focus:outline-none focus:ring-4 focus:ring-indigo-600" href="/">
+      <img className="h-8" src={ Logo } alt="Open" />
+    </a>
+    <div className="flex gap-3 md:gap-6">
+      <a className="rounded-lg focus:outline-none focus:ring-4 focus:ring-neutral-300 text-neutral-300 hover:text-white focus:text-white text-sm md:text-base px-4 md:px-5 py-2 md:py-2" href="/">Documentation</a>
+      <a className="bg-indigo-600 hover:bg-indigo-700 focus:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-600 rounded-lg text-white text-sm md:text-base font-medium px-4 md:px-5 py-2 md:py-2" href="/">Sign up</a>
+    </div>
+  </nav>
+)
+
+export default Navbar
