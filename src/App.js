8de1a96b2d3dfbdc6ab2746afd7210379398388d
aliiev <aliiev.akhtem@gmail.com>
2022-06-26 20:08:15 +0300

    Created Features section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
+import Features from './sections/Features'
 
 const App = () => (
   <div className="container mx-auto mt-8 px-6 md:px-0">
     <Navbar />
     <Hero />
+    <Features />
   </div>
 )
 
